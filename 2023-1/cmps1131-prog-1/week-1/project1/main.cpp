3fe90d39777c700133742f4f24276de39cea134d
andreshungbz <andres.hung@outlook.com>
2023-08-20 20:04:55 -0600

    2023/08/20 update

---
+++ 2023-1/cmps1131-prog-1/week-1/project1/main.cpp
@@ -0,0 +1,19 @@
+/* Author: Vernelle Sylvester
+ * Program name: My first progam
+ * Purpose:My first program
+ * Date: 27/1/18
+ */
+
+// headers:
+#include <iostream>
+
+using namespace std;
+
+
+/* Main program */
+
+int main()
+{
+    cout <<"My first program" << endl;
+    return 0;
+}
