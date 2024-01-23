a516b27204f711a4f390b334d73fd74a81a93f6c
Andres Hung <andres.hung@outlook.com>
2024-01-23 10:27:02 -0600

    2024/01/23 update

---
+++ 2023-2/CMPS1232/practice/reverse-arr/main.cpp
@@ -0,0 +1,48 @@
+#include <array>
+#include <iostream>
+#include <utility> // for std::swap
+
+template <typename T, std::size_t N>
+std::array<T, N> reverseArray(std::array<T, N> arr);
+
+int main() {
+    const int arrSize{10};
+    std::array<int, arrSize> numbers{
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+
+    std::array<int, arrSize> numbersCopy{reverseArray(numbers)};
+
+    // boolean used to avoid printing a trailing space at the end of the loop
+    bool first{true};
+
+    for (const int number : numbersCopy) {
+        if (first) {
+            first = false;
+        } else {
+            std::cout << ' ';
+        }
+
+        std::cout << number;
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
+
+// array is copied twice but integers are relatively cheap to copy
+template <typename T, std::size_t N>
+std::array<T, N> reverseArray(std::array<T, N> arr) {
+    // for keeping track of array from the end
+    std::size_t sizeIndex{arr.size() - 1};
+    // only half the array needs to be traversed to swap the values
+    // truncation works for arrays with odd number of items (middle items doesn't need any swap)
+    const std::size_t stopIndex{arr.size() / 2};
+
+    for (std::size_t i{0}; i < stopIndex; ++i) {
+        std::swap(arr[i], arr[sizeIndex--]);
+    }
+
+    return arr;
+}
