1b9e6757c311f4092c12855a152a29b6f1c8baaf
Andres Hung <andres.hung@outlook.com>
2024-02-12 10:50:56 -0600

    2024/02/12 update

--- 2023-2/CMPS1232/quizzes/quiz-2/rm-duplicate.cpp
+++ 2023-2/CMPS1232/quizzes/quiz-2/rm-duplicate.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 
-bool inArray(const int arr[], int num, std::size_t len);
-void eliminate_duplicates(int arr[], std::size_t len);
+template <typename T>
+void printArray(const T arr[], std::size_t len);
+
+template <typename T>
+bool inArray(const T arr[], T element, std::size_t len);
+
+template <typename T>
+void eliminate_duplicates(T arr[], std::size_t len);
 
 int main() {
     const std::size_t size{5};
     int array[size] {1, 2, 1, 5, 2};
+
+    std::cout << "Before: ";
+    printArray(array, size);
+    std::cout << '\n';
 
     std::cout << "After: ";
     eliminate_duplicates(array, size);
@@ -14,28 +24,41 @@ int main() {
     return 0;
 }
 
-bool inArray(const int arr[], int num, std::size_t len) {
+template <typename T>
+void printArray(const T arr[], std::size_t len) {
+    bool first{true};
+
     for (std::size_t i{0}; i < len; ++i) {
-        if (arr[i] == num) {
+        if (first)
+            first = false;
+        else
+            std::cout << ' ';
+
+        std::cout << arr[i];
+    }
+}
+
+template <typename T>
+bool inArray(const T arr[], T element, std::size_t len) {
+    for (std::size_t i{0}; i < len; ++i) {
+        if (arr[i] == element)
             return true;
-        }
     }
 
     return false;
 }
 
-void eliminate_duplicates(int arr[], std::size_t len) {
-    std::size_t arrCounter{0};
-    int newArray[len];
+template <typename T>
+void eliminate_duplicates(T arr[], std::size_t len) {
+    std::size_t index{0};
+    T newArray[len];
 
     for (std::size_t i{0}; i < len; ++i) {
-        if (!inArray(newArray, arr[i], arrCounter)) {
-            newArray[arrCounter] = arr[i];
-            ++arrCounter;
+        if (!inArray(newArray, arr[i], index)) {
+            newArray[index] = arr[i];
+            ++index;
         }
     }
 
-    for (std::size_t i{0}; i < arrCounter; ++i) {
-        std::cout << newArray[i] << ' ';
-    }
+    printArray(newArray, index);
 }
